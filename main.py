52406e90f6c945b9b1fa47cf5306dfb2ef424c2f
Rangga Prangwedana <vergil1st@gmail.com>
2025-12-19 19:33:13 +0700

    fix: Fixed load tasks function

--- main.py
+++ main.py
@@ -1,23 +1,64 @@
 import os
 import json
 from datetime import datetime
+from tracemalloc import start
+from typing import List, Dict, Optional
 
 FILENAME = "tasks.json"
+VALID_STATUSES = ["todo", "in-progress", "done"]
 
-def load_tasks():
+def load_tasks() -> List(Dict):
     """
     Load tasks from local JSON file. Return empty list if file not found or invalid.
+    If old format detected, migrate it.
     """
 
     if not os.path.exists(FILENAME):
         return []
 
     try:
-        with open(FILENAME, "r") as file:
-            return json.load(file)
-    except (json.JSONDecodeError, FileNotFoundError):
-        print("WARNING! Failed to read tasks.json. Starting with an empty list.")
+        with open(FILENAME, "r", encoding="utf-8") as file:
+            data = json.load(file)
+    except (json.JSONDecodeError, FileNotFoundError) as e:
+        print(f"WARNING! Failed to read {FILENAME}: {e}. Starting with an empty list.")
         return []
+    
+    # Validate list data
+    if not isinstance(data, list): 
+        print(f"{FILENAME} format unrecognized. Starting with an empty list.")
+        return []
+
+    
+    # Detect old format files and migrate
+    migrated = False
+    new_list = []
+    for item in data:
+        if isinstance(item, dict) and "task" in item and "done" in item:
+            status = "done" if item.get("done") else "todo"
+            now = timestamp_now()
+            new_item = {
+                "id": None, # Only temporary, assign ID after loop
+                "description": item.get("task", "").strip(),
+                "status": status,
+                "createdAt": item.get("createdAt", now),
+                "updatedAt": item.get("updatedAt", now),
+            }
+            new_list.append(new_item)
+            migrated = True
+        elif isinstance(item, dict) and "id" in item and "description" in item:
+            new_list.append(item)
+        else:
+            continue
+
+    if migrated:
+        for idx, itm in enumerate(new_list, start=1):
+            itm["id"] = idx
+        save_tasks(new_list)
+        print("Migrated tasks from old format to the new format.")
+
+    return new_list
+
+
 
 
 def save_tasks(tasks):
@@ -32,9 +73,9 @@ def save_tasks(tasks):
         print(f"Error saving task: {e}")
 
 
-def timestamp_now():
+def timestamp_now() -> str:
     """
-    Return current timestamp as a string.
+    Return current timestamp as a string in ISO format.
     """
     return datetime.now().isoformat(timespec="seconds")
 
